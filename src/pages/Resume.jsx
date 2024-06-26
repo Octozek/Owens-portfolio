c8c441fca0c00228ae5443f79db0921a2beeee3a
Octozek <octozek@gmail.com>
2024-06-25 19:32:12 -0500

    Add download link for resume in Resume component

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,30 +4,37 @@ function Resume() {
   return (
     <section className="resume-section">
       <h2>Resume</h2>
-      <a href="//resume.pdf" className="download-resume" download>
+      <p>
+        My resume is currently being updated. I am awaiting information from my current job.
+      </p>
+      <a href="/resume.pdf" className="download-resume" download>
         Download Resume
       </a>
-      <div className="proficiency-section">
-        <h3>Front-end Proficiencies</h3>
-        <ul className="proficiency-list">
-          <li>HTML & CSS</li>
-          <li>JavaScript</li>
-          <li>jQuery</li>
-          <li>Responsive Design</li>
-          <li>React</li>
-          <li>Bootstrap</li>
-        </ul>
-      </div>
-      <div className="proficiency-section">
-        <h3>Back-end Proficiencies</h3>
-        <ul className="proficiency-list">
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>MySQL, Sequelize</li>
-          <li>REST</li>
-          <li>GraphQL</li>
-        </ul>
+      <h3>Proficiencies</h3>
+      <div className="proficiencies">
+        <div className="proficiency-category">
+          <h4>Front-end Proficiencies</h4>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>Responsive Design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
+        <div className="proficiency-category">
+          <h4>Back-end Proficiencies</h4>
+          <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
       </div>
     </section>
   );
