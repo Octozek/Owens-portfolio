3d16a9d53ac4bc493803d664b2bf5a51b6ba7344
Octozek <octozek@gmail.com>
2024-06-25 19:24:02 -0500

    Add professional message for resume status and update Resume component

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,31 +4,32 @@ function Resume() {
   return (
     <section className="resume-section">
       <h2>Resume</h2>
-      <a href="/path/to/your/resume.pdf" className="download-resume" download>
-        Download Resume
+      <p className="resume-message">
+        My resume is currently being updated with the latest information from my current job. It will be available soon.
+      </p>
+      <a href="#" className="download-resume" onClick={(e) => e.preventDefault()}>
+        Download Resume (Coming Soon)
       </a>
-      <div className="proficiency-section">
-        <h3>Front-end Proficiencies</h3>
-        <ul className="proficiency-list">
-          <li>HTML & CSS</li>
-          <li>JavaScript</li>
-          <li>jQuery</li>
-          <li>Responsive Design</li>
-          <li>React</li>
-          <li>Bootstrap</li>
-        </ul>
-      </div>
-      <div className="proficiency-section">
-        <h3>Back-end Proficiencies</h3>
-        <ul className="proficiency-list">
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>MySQL, Sequelize</li>
-          <li>REST</li>
-          <li>GraphQL</li>
-        </ul>
-      </div>
+      <h3>Front-end Proficiencies</h3>
+      <ul className="proficiency-list">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>Responsive Design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+      <h3>Back-end Proficiencies</h3>
+      <ul className="proficiency-list">
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+        <li>Blender</li>
+      </ul>
     </section>
   );
 }
