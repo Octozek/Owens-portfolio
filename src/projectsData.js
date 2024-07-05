01c4fece473bd9e8500a6e3d9e03b66fdd38bb1e
Octozek <octozek@gmail.com>
2024-07-05 16:51:05 -0500

    Add BookFinder-GraphQL and codequiz projects to portfolio

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,17 +1,31 @@
 const projects = [
   {
-    image: '/assets/images/WeatherWhiz.png',
+    image: '/src/assets/images/WeatherWhiz.png',
     repoLink: 'https://github.com/Octozek/WeatherWhiz',
     websiteLink: 'https://octozek.github.io/WeatherWhiz/',
     repoName: 'WeatherWhiz',
     websiteName: 'WeatherWhiz',
   },
   {
-    image: '/assets/images/RecipeRoamer.png',
+    image: '/src/assets/images/RecipeRoamer.png',
     repoLink: 'https://github.com/Octozek/RecipeRoamer',
     websiteLink: 'https://octozek.github.io/RecipeRoamer/',
     repoName: 'RecipeRoamer',
     websiteName: 'RecipeRoamer',
+  },
+  {
+    image: '/src/assets/images/G-falls.png',
+    repoLink: 'https://github.com/Octozek/BookFinder-GraphQL',
+    websiteLink: 'https://bookfinder-graphql-frontend.onrender.com/',
+    repoName: 'BookFinder-GraphQL',
+    websiteName: 'BookFinder-GraphQL',
+  },
+  {
+    image: '/src/assets/images/J-Q.png',
+    repoLink: 'https://github.com/Octozek/codequiz',
+    websiteLink: 'https://octozek.github.io/codequiz/',
+    repoName: 'codequiz',
+    websiteName: 'codequiz',
   },
   // Add more projects as needed
 ];
